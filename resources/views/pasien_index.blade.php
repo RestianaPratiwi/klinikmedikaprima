1495cfa98208c8a075857863d510f9f032d82900
Restiana <restianapratiwi324@gmail.com>
2024-10-01 11:20:35 +0700

    modifikasi table

--- resources/views/pasien_index.blade.php
+++ resources/views/pasien_index.blade.php
@@ -1,10 +1,34 @@
 @extends('mylayout', ['tittle' => 'Data Pasien'])
-    @section('content')
-        <div class="card">
-            <div clasas="card-body">
-                <h5 class="card-tittle">Data Pasien</h5>
-                Ini data pasien
-                </div>
-            </div>
-    @endsection
-    
+@section('content')
+   <div class="card">
+        <div class="card-body">
+            <h3>Data Pasien</h3>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>NO</th>
+                        <th>NO PASIEN</th>
+                        <th>NAMA</th>
+                        <th>UMUR</th>
+                        <th>JENIS KELAMIN</th>
+                        <th>TANGGAL BUAT</th>
+                        <th>AKSI</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($pasien as $item)
+                        <tr>
+                           <td>{{ $loop->iteration }}</td>
+                           <td>{{ $item->no_pasien }}</td>
+                           <td>{{ $item->nama }}</td>
+                           <td>{{ $item->umur }}</td>
+                           <td>{{ $item->jenis_kelamin }}</td>
+                           <td>{{ $item->created_at }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {!! $pasien->links() !!}
+        </div>
+    </div>
+@endsection
