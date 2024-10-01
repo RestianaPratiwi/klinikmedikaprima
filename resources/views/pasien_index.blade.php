35fa1c9bb940255d70df2f1bdbadfe626d6473e6
Restiana <restianapratiwi324@gmail.com>
2024-10-01 10:49:48 +0700

    commit pertama

---
+++ resources/views/pasien_index.blade.php
@@ -0,0 +1,10 @@
+@extends('mylayout', ['tittle' => 'Data Pasien'])
+    @section('content')
+        <div class="card">
+            <div clasas="card-body">
+                <h5 class="card-tittle">Data Pasien</h5>
+                Ini data pasien
+                </div>
+            </div>
+    @endsection
+    
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\PasienController;
+
+Route::resource('pasien', PasienController::class);
+
+Route::get('profile', function (){
+    return 'hello world';
+});
 
 Route::get('/', function () {
     return view('welcome');
